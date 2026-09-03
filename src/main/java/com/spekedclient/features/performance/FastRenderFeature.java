bb8053d2ff5a45e4f44afe8340e1a7a084479abc
Viper Davis <viperxc2204@gmail.com>
2026-09-03 21:01:06 +0100

    Use public max FPS accessor

--- src/main/java/com/spekedclient/features/performance/FastRenderFeature.java
+++ src/main/java/com/spekedclient/features/performance/FastRenderFeature.java
@@ -4,23 +4,9 @@ import com.spekedclient.features.Feature;
 import net.minecraft.client.gui.DrawContext;
 
 public class FastRenderFeature extends Feature {
-    public FastRenderFeature() {
-        super("Fast Render", "Optimize render pipeline", FeatureCategory.PERFORMANCE);
-    }
-
-    @Override
-    public void onEnable() {
-        if (mc != null && mc.options != null) {
-            mc.options.maxFps.setValue(300);
-        }
-    }
-
-    @Override
-    public void onDisable() {}
-
-    @Override
-    public void onTick() {}
-
-    @Override
-    public void onRender(DrawContext context, float tickDelta) {}
+    public FastRenderFeature() { super("Fast Render", "Optimize render pipeline", FeatureCategory.PERFORMANCE); }
+    @Override public void onEnable() { if (mc != null && mc.options != null) mc.options.getMaxFps().setValue(300); }
+    @Override public void onDisable() {}
+    @Override public void onTick() {}
+    @Override public void onRender(DrawContext context, float tickDelta) {}
 }
