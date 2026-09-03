4b68c87ff3f54a87fb29840e2e68843311a7090f
Viper Davis <viperxc2204@gmail.com>
2026-09-03 21:00:44 +0100

    Fix FOV option type for 1.21.11

--- src/main/java/com/spekedclient/features/visual/ZoomFeature.java
+++ src/main/java/com/spekedclient/features/visual/ZoomFeature.java
@@ -5,36 +5,14 @@ import net.minecraft.client.gui.DrawContext;
 
 public class ZoomFeature extends Feature {
     private float zoomLevel = 3.0f;
-    private float originalFov = 0;
-
-    public ZoomFeature() {
-        super("Zoom", "Zoom in with keybind", FeatureCategory.VISUAL);
-    }
-
-    @Override
-    public void onEnable() {
-        if (mc != null) {
-            originalFov = mc.options.getFov().getValue().floatValue();
-        }
-    }
-
-    @Override
-    public void onDisable() {
-        if (mc != null) {
-            mc.options.getFov().setValue((double) originalFov);
-        }
-    }
-
-    @Override
-    public void onTick() {
+    private int originalFov = 70;
+    public ZoomFeature() { super("Zoom", "Zoom in with keybind", FeatureCategory.VISUAL); }
+    @Override public void onEnable() { if (mc != null) originalFov = mc.options.getFov().getValue(); }
+    @Override public void onDisable() { if (mc != null) mc.options.getFov().setValue(originalFov); }
+    @Override public void onTick() {
         if (mc == null) return;
-        if (mc.options.sprintKey.isPressed()) {
-            mc.options.getFov().setValue((double) (originalFov / zoomLevel));
-        } else {
-            mc.options.getFov().setValue((double) originalFov);
-        }
+        if (mc.options.sprintKey.isPressed()) mc.options.getFov().setValue(Math.max(1, (int) (originalFov / zoomLevel)));
+        else mc.options.getFov().setValue(originalFov);
     }
-
-    @Override
-    public void onRender(DrawContext context, float tickDelta) {}
+    @Override public void onRender(DrawContext context, float tickDelta) {}
 }
