d15d01e2a286d7a4ca83d9c5999b0f7b9defd13c
Viper Davis <viperxc2204@gmail.com>
2026-09-03 21:01:01 +0100

    Use public max FPS accessor

--- src/main/java/com/spekedclient/features/performance/FpsLimiterFeature.java
+++ src/main/java/com/spekedclient/features/performance/FpsLimiterFeature.java
@@ -5,28 +5,9 @@ import net.minecraft.client.gui.DrawContext;
 
 public class FpsLimiterFeature extends Feature {
     private int fpsLimit = 60;
-
-    public FpsLimiterFeature() {
-        super("FPS Limiter", "Cap FPS at custom value", FeatureCategory.PERFORMANCE);
-    }
-
-    @Override
-    public void onEnable() {
-        if (mc != null) {
-            mc.options.maxFps.setValue(fpsLimit);
-        }
-    }
-
-    @Override
-    public void onDisable() {
-        if (mc != null) {
-            mc.options.maxFps.setValue(260);
-        }
-    }
-
-    @Override
-    public void onTick() {}
-
-    @Override
-    public void onRender(DrawContext context, float tickDelta) {}
+    public FpsLimiterFeature() { super("FPS Limiter", "Cap FPS at custom value", FeatureCategory.PERFORMANCE); }
+    @Override public void onEnable() { if (mc != null) mc.options.getMaxFps().setValue(fpsLimit); }
+    @Override public void onDisable() { if (mc != null) mc.options.getMaxFps().setValue(260); }
+    @Override public void onTick() {}
+    @Override public void onRender(DrawContext context, float tickDelta) {}
 }
