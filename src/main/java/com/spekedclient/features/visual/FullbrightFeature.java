dd145b1f17ff4d058f1ac05f207487efbaa85ee6
Viper Davis <viperxc2204@gmail.com>
2026-09-03 21:00:40 +0100

    Use public GameOptions gamma accessor

--- src/main/java/com/spekedclient/features/visual/FullbrightFeature.java
+++ src/main/java/com/spekedclient/features/visual/FullbrightFeature.java
@@ -4,30 +4,10 @@ import com.spekedclient.features.Feature;
 import net.minecraft.client.gui.DrawContext;
 
 public class FullbrightFeature extends Feature {
-    private float originalGamma = 0;
-
-    public FullbrightFeature() {
-        super("Fullbright", "Full brightness everywhere", FeatureCategory.VISUAL);
-    }
-
-    @Override
-    public void onEnable() {
-        if (mc != null) {
-            originalGamma = mc.options.gamma.getValue().floatValue();
-            mc.options.gamma.setValue(16.0);
-        }
-    }
-
-    @Override
-    public void onDisable() {
-        if (mc != null) {
-            mc.options.gamma.setValue((double) originalGamma);
-        }
-    }
-
-    @Override
-    public void onTick() {}
-
-    @Override
-    public void onRender(DrawContext context, float tickDelta) {}
+    private double originalGamma = 0;
+    public FullbrightFeature() { super("Fullbright", "Full brightness everywhere", FeatureCategory.VISUAL); }
+    @Override public void onEnable() { if (mc != null) { originalGamma = mc.options.getGamma().getValue(); mc.options.getGamma().setValue(16.0); } }
+    @Override public void onDisable() { if (mc != null) mc.options.getGamma().setValue(originalGamma); }
+    @Override public void onTick() {}
+    @Override public void onRender(DrawContext context, float tickDelta) {}
 }
