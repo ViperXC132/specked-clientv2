2c1f4a9d27d3ecc97ca27888a6465a6e2ba13452
Viper Davis <viperxc2204@gmail.com>
2026-09-03 21:01:03 +0100

    Use world simulation distance API

--- src/main/java/com/spekedclient/features/performance/SmartRenderFeature.java
+++ src/main/java/com/spekedclient/features/performance/SmartRenderFeature.java
@@ -4,25 +4,12 @@ import com.spekedclient.features.Feature;
 import net.minecraft.client.gui.DrawContext;
 
 public class SmartRenderFeature extends Feature {
-    public SmartRenderFeature() {
-        super("Smart Render", "Intelligent render distance", FeatureCategory.PERFORMANCE);
+    public SmartRenderFeature() { super("Smart Render", "Intelligent render distance", FeatureCategory.PERFORMANCE); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+    @Override public void onTick() {
+        if (mc == null || mc.options == null || mc.world == null) return;
+        if (mc.isWindowFocused()) mc.world.setSimulationDistance(Math.max(8, mc.world.getSimulationDistance()));
     }
-
-    @Override
-    public void onEnable() {}
-
-    @Override
-    public void onDisable() {}
-
-    @Override
-    public void onTick() {
-        if (mc == null || mc.options == null) return;
-        if (mc.isWindowFocused()) {
-            int currentDist = mc.options.simulationDistance.getValue();
-            mc.options.simulationDistance.setValue(Math.max(8, currentDist));
-        }
-    }
-
-    @Override
-    public void onRender(DrawContext context, float tickDelta) {}
+    @Override public void onRender(DrawContext context, float tickDelta) {}
 }
