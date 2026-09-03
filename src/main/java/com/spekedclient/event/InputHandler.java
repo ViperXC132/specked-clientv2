f469ea4a367e5243ecfaef4b7eebc137e520c62d
Viper Davis <viperxc2204@gmail.com>
2026-09-03 21:00:18 +0100

    Fix KeyBinding category API for Minecraft 1.21.11

--- src/main/java/com/spekedclient/event/InputHandler.java
+++ src/main/java/com/spekedclient/event/InputHandler.java
@@ -1,57 +1,28 @@
 package com.spekedclient.event;
 
-import com.spekedclient.SpekedClient;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
 
 public class InputHandler {
+    private static final KeyBinding.Category CATEGORY = KeyBinding.Category.create(Identifier.of("spekedclient", "keybinds"));
     private static KeyBinding clickguiKey;
     private static KeyBinding huiEditorKey;
     private static KeyBinding zoomKey;
 
     public static void init() {
-        clickguiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-            "key.spekedclient.clickgui",
-            GLFW.GLFW_KEY_RIGHT_SHIFT,
-            "Speked Client"
-        ));
-
-        huiEditorKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-            "key.spekedclient.huieditor",
-            GLFW.GLFW_KEY_H,
-            "Speked Client"
-        ));
-
-        zoomKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-            "key.spekedclient.zoom",
-            GLFW.GLFW_KEY_C,
-            "Speked Client"
-        ));
-
+        clickguiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.spekedclient.clickgui", GLFW.GLFW_KEY_RIGHT_SHIFT, CATEGORY));
+        huiEditorKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.spekedclient.huieditor", GLFW.GLFW_KEY_H, CATEGORY));
+        zoomKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.spekedclient.zoom", GLFW.GLFW_KEY_C, CATEGORY));
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (clickguiKey.wasPressed()) {
-                handleClickGUI();
-            }
-            if (huiEditorKey.wasPressed()) {
-                handleHUDEditor();
-            }
-            if (zoomKey.isPressed()) {
-                handleZoom();
-            }
+            if (clickguiKey.wasPressed()) handleClickGUI();
+            if (huiEditorKey.wasPressed()) handleHUDEditor();
+            if (zoomKey.isPressed()) handleZoom();
         });
     }
-
-    private static void handleClickGUI() {
-        // TODO: Open ClickGUI screen
-    }
-
-    private static void handleHUDEditor() {
-        // TODO: Open HUD editor screen
-    }
-
-    private static void handleZoom() {
-        // TODO: Handle zoom feature
-    }
+    private static void handleClickGUI() {}
+    private static void handleHUDEditor() {}
+    private static void handleZoom() {}
 }
