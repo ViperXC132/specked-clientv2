b3929db86920a2840621e4c08ff46be5e6d8e1b5
Viper Davis <viperxc2204@gmail.com>
2026-09-03 21:00:57 +0100

    Use public max FPS accessor

--- src/main/java/com/spekedclient/features/performance/DynamicFpsFeature.java
+++ src/main/java/com/spekedclient/features/performance/DynamicFpsFeature.java
@@ -4,27 +4,9 @@ import com.spekedclient.features.Feature;
 import net.minecraft.client.gui.DrawContext;
 
 public class DynamicFpsFeature extends Feature {
-    public DynamicFpsFeature() {
-        super("Dynamic FPS", "Adjust FPS dynamically", FeatureCategory.PERFORMANCE);
-    }
-
-    @Override
-    public void onEnable() {}
-
-    @Override
-    public void onDisable() {}
-
-    @Override
-    public void onTick() {
-        if (mc == null) return;
-        
-        if (mc.isWindowFocused()) {
-            mc.options.maxFps.setValue(240);
-        } else {
-            mc.options.maxFps.setValue(30);
-        }
-    }
-
-    @Override
-    public void onRender(DrawContext context, float tickDelta) {}
+    public DynamicFpsFeature() { super("Dynamic FPS", "Adjust FPS dynamically", FeatureCategory.PERFORMANCE); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+    @Override public void onTick() { if (mc != null) mc.options.getMaxFps().setValue(mc.isWindowFocused() ? 240 : 30); }
+    @Override public void onRender(DrawContext context, float tickDelta) {}
 }
